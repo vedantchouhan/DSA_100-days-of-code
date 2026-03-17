fe263e51571bd2a0a90d2126ad5950d66134167d
vedantchouhan <chouhanvedant84@gmail.com>
2026-03-17 21:50:56 +0530

    Linear Search with Comparison Count

---
+++ Day-3/q3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i;
+    
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    scanf("%d", &k);
+    
+    int comparisons = 0;
+    int found = 0;
+    
+    for(i = 0; i < n; i++) {
+        comparisons++;
+        
+        if(arr[i] == k) {
+            printf("Found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
+    
+    if(!found) {
+        printf("Not Found\n");
+    }
+    
+    printf("Comparisons = %d\n", comparisons);
+    
+    return 0;
+}
