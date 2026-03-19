558b4949fa4b0cafe187431473f042236b83205d
vedantchouhan <chouhanvedant84@gmail.com>
2026-03-19 13:36:44 +0530

    mirror the code name

---
+++ Day-9/q9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char str[], int start, int end) {
+    if (start >= end)
+        return;
+
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+
+    reverse(str, start + 1, end - 1);
+}
+
+int main() {
+    char str[100];
+
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    reverse(str, 0, len - 1);
+
+    printf("%s", str);
+
+    return 0;
+}
