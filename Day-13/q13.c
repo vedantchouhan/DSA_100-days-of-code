676857bbdc500327521b83f0d0bf486b933f4fbe
vedantchouhan <chouhanvedant84@gmail.com>
2026-03-20 11:41:11 +0530

    boundary to centre matrix walk

---
+++ Day-13/q13.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int A[r][c];
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    int top = 0, bottom = r - 1;
+    int left = 0, right = c - 1;
+
+    while (top <= bottom && left <= right) {
+
+        for (int j = left; j <= right; j++) {
+            printf("%d ", A[top][j]);
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++) {
+            printf("%d ", A[i][right]);
+        }
+        right--;
+
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--) {
+                printf("%d ", A[bottom][j]);
+            }
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                printf("%d ", A[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return 0;
+}
