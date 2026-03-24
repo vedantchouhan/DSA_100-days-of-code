6b7930e4993b75caa75bed2ed91e7783cdb5afb1
vedantchouhan <chouhanvedant84@gmail.com>
2026-03-24 08:35:06 +0530

    Stack Implementation using Array

---
+++ Day-31/q31.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    top++;
+    stack[top] = value;
+}
+
+// Pop operation
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("%d\n", stack[top]);
+    top--;
+}
+
+// Display operation
+void display() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    for (int i = top; i >= 0; i--) {
+        printf("%d", stack[i]);
+        if (i != 0) printf(" ");
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, op, value;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &op);
+
+        if (op == 1) {
+            scanf("%d", &value);
+            push(value);
+        }
+        else if (op == 2) {
+            pop();
+        }
+        else if (op == 3) {
+            display();
+        }
+    }
+
+    return 0;
+}
