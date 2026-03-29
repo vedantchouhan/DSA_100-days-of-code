98882c66bd2b30f82efc2fcea43804d84ae9241f
vedantchouhan <chouhanvedant84@gmail.com>
2026-03-29 13:35:01 +0530

    Level Order Traversal

---
+++ Day-46/q46.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of a tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Queue for level order traversal
+struct Queue {
+    struct Node* arr[1000];
+    int front, rear;
+};
+
+void initQueue(struct Queue* q) {
+    q->front = q->rear = -1;
+}
+
+int isEmpty(struct Queue* q) {
+    return q->front == -1;
+}
+
+void enqueue(struct Queue* q, struct Node* node) {
+    if (q->rear == 999) return;
+    if (q->front == -1) q->front = 0;
+    q->arr[++q->rear] = node;
+}
+
+struct Node* dequeue(struct Queue* q) {
+    if (isEmpty(q)) return NULL;
+    struct Node* temp = q->arr[q->front];
+    if (q->front == q->rear)
+        q->front = q->rear = -1;
+    else
+        q->front++;
+    return temp;
+}
+
+// Level Order Traversal
+void levelOrder(struct Node* root) {
+    if (root == NULL) return;
+
+    struct Queue q;
+    initQueue(&q);
+
+    enqueue(&q, root);
+
+    while (!isEmpty(&q)) {
+        struct Node* curr = dequeue(&q);
+        printf("%d ", curr->data);
+
+        if (curr->left) enqueue(&q, curr->left);
+        if (curr->right) enqueue(&q, curr->right);
+    }
+}
+
+int main() {
+    int n, val;
+    scanf("%d", &n);
+
+    if (n == 0) return 0;
+
+    struct Node* nodes[1000];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        if (val == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(val);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+
+            if (leftIndex < n)
+                nodes[i]->left = nodes[leftIndex];
+            if (rightIndex < n)
+                nodes[i]->right = nodes[rightIndex];
+        }
+    }
+
+    levelOrder(nodes[0]);
+
+    return 0;
+}
