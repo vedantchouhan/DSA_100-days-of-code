b65e436f143668543a3e27be18503d491961cdbd
vedantchouhan <chouhanvedant84@gmail.com>
2026-03-29 13:36:03 +0530

    Height of Binary Tree

---
+++ Day-47/q47.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Height function
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    int n, val;
+    scanf("%d", &n);
+
+    if (n == 0) {
+        printf("0");
+        return 0;
+    }
+
+    struct Node* nodes[1000];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        if (val == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(val);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+
+            if (leftIndex < n)
+                nodes[i]->left = nodes[leftIndex];
+            if (rightIndex < n)
+                nodes[i]->right = nodes[rightIndex];
+        }
+    }
+
+    printf("%d", height(nodes[0]));
+
+    return 0;
+}
