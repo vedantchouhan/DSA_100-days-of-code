6cbee663bea22ca5aceed5e7cc3a2de53676cab9
vedantchouhan <chouhanvedant84@gmail.com>
2026-04-06 21:02:44 +0530

    Cycle Detection in Undirected Graph (DFS)

---
+++ Day-65/q65.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX 1000
+
+int adj[MAX][MAX];
+bool visited[MAX];
+
+bool dfs(int node, int parent, int n) {
+    visited[node] = true;
+
+    for (int i = 0; i < n; i++) {
+        if (adj[node][i]) {
+            if (!visited[i]) {
+                if (dfs(i, node, n))
+                    return true;
+            } else if (i != parent) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int hasCycle(int n) {
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            if (dfs(i, -1, n))
+                return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            adj[i][j] = 0;
+
+    for (int i = 0; i < n; i++)
+        visited[i] = false;
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    if (hasCycle(n))
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
