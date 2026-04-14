64b4afdfab9d907cf847e05370d66553f24396ec
vedantchouhan <chouhanvedant84@gmail.com>
2026-04-14 16:10:01 +0530

    Bellman-Ford Algorithm

---
+++ Day-70/q70.c
@@ -0,0 +1,62 @@
+//70.
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 1000
+
+typedef struct {
+    int u, v, w;
+} Edge;
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    Edge edges[MAX];
+
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
+    }
+
+    int src;
+    scanf("%d", &src);
+
+    int dist[MAX];
+
+    for (int i = 0; i < n; i++)
+        dist[i] = INT_MAX;
+
+    dist[src] = 0;
+
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = 0; j < m; j++) {
+            int u = edges[j].u;
+            int v = edges[j].v;
+            int w = edges[j].w;
+
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+
+    for (int j = 0; j < m; j++) {
+        int u = edges[j].u;
+        int v = edges[j].v;
+        int w = edges[j].w;
+
+        if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+            printf("NEGATIVE CYCLE\n");
+            return 0;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (dist[i] == INT_MAX)
+            printf("INF ");
+        else
+            printf("%d ", dist[i]);
+    }
+
+    return 0;
+}
