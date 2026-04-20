8b9c9ba11a0abcf0754d94b4d718d5ce7c0515b2
vedantchouhan <chouhanvedant84@gmail.com>
2026-04-20 19:33:47 +0530

    Count Connected Components (Undirected Graph)

---
+++ Day-76/q76.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+int visited[MAX];
+int adj[MAX][MAX];
+int n, m;
+
+// DFS function
+void dfs(int node) {
+    visited[node] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    int u, v;
+
+    // initialize adjacency matrix
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    // initialize visited array
+    for (int i = 1; i <= n; i++) {
+        visited[i] = 0;
+    }
+
+    // input edges
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // undirected graph
+    }
+
+    int components = 0;
+
+    // count connected components
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+
+    return 0;
+}
